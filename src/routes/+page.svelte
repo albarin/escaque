930515f9b284d7aada44943182a288d8e11effc3
Alba Rincón <alba@rincon.me>
2026-02-08 20:21:52 +0100

    Add selected piece background

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,16 +2,35 @@
 	import { getPieceImage, initBoard, type Piece } from '$lib';
 
 	const board: (Piece | null)[][] = initBoard();
+
+	let selectedPiece: { piece: Piece; x: number; y: number } | null = $state(null);
+
+	function selectPiece(piece: Piece, x: number, y: number) {
+		selectedPiece = { piece, x, y };
+	}
+
+	function isSelected(x: number, y: number) {
+		return selectedPiece && selectedPiece.x === x && selectedPiece.y === y;
+	}
 </script>
 
-<div class="mx-auto mt-20 w-160 border-8 border-cyan-950">
+<div class="mx-auto mt-20 w-160 border-10 border-cyan-950">
 	{#each board as row, i}
 		<div class="row">
 			{#each row as _, j}
-				<div class="cell {(i + j) % 2 === 0 ? 'white' : 'black'}">
+				<div
+					class={[
+						'cell',
+                        {
+                            selected: isSelected(i, j),
+                            white: !isSelected(i, j) && (i + j) % 2 === 0,
+                            black: !isSelected(i, j) && (i + j) % 2 !== 0
+                        }
+					]}
+				>
 					{#if board[i][j]}
-						<button class="cursor-pointer">
-							<img class="w-16" src={getPieceImage(board[i][j])} alt={board[i][j]?.type} />
+						<button class="cursor-pointer" onclick={() => selectPiece(board[i][j]!, i, j)}>
+							<img class="w-17" src={getPieceImage(board[i][j])} alt={board[i][j]?.type} />
 						</button>
 					{/if}
 				</div>
@@ -41,5 +60,9 @@
 	.cell.black {
 		background-color: #666;
 		color: white;
+	}
+
+	.selected {
+		background-color: rgb(150, 192, 255);
 	}
 </style>
